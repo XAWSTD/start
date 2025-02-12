5cbeae9d4f0cfde4ff6e2aa23196310c493bd79a
XAWSTD <js54144@gmail.com>
2025-02-12 11:50:26 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,4 @@ sudo ifconfig eth0 up ;
 cd /etc/openvpn ;
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
-sudo openvpn us_texas.ovpn ;
-
-sudo wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.6_all.deb ;
-sudo dpkg -i ./protonvpn-stable-release_1.0.6_all.deb && sudo apt update ; 
-sudo apt install proton-vpn-gnome-desktop
+sudo openvpn us_texas.ovpn &
