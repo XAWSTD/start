51786b2d4b20a793bc758801cbea262ad87e2c95
XAWSTD <js54144@gmail.com>
2025-02-12 12:30:55 -0500

    Update and rename peeiA to peeiA.sh

--- peeiA.sh
+++ peeiA.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd /etc/openvpn ;
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
-sudo openvpn us_texas.ovpn &
+sudo openvpn us_texas.ovpn
