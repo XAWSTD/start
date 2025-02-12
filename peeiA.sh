78543751c8e524699ae10186bf22175cc0647ef3
XAWSTD <js54144@gmail.com>
2025-02-12 12:39:57 -0500

    Update peeiA.sh

--- peeiA.sh
+++ peeiA.sh
@@ -3,4 +3,4 @@
 cd /etc/openvpn ;
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
-sudo openvpn us_texas.ovpn
+sudo openvpn --auth-nocache us_texas.ovpn
