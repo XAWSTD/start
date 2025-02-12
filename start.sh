59076f88973a0923cba1006e49c188d7a57acde6
XAWSTD <js54144@gmail.com>
2025-02-12 12:05:14 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+cd /etc/openvpn ;
+sudo mv /home/kali/start/virtpncon.ovpn /etc/ovpn ; 
+sudo chmod 664 virtpncon.ovpn ;
 sudo ifconfig eth0 down ; 
 sudo macchanger eth0 -A; 
 sudo macchanger eth0 -rb ; 
 sudo ifconfig eth0 up ;  
-
-cd /etc/openvpn ;
+sudo openvpn virtpncon.ovpn &
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
 sudo openvpn us_texas.ovpn &
