aaf2f19f8f271f1d57d7c332e7806c61eee8cea5
XAWSTD <js54144@gmail.com>
2025-02-12 12:12:43 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd /etc/openvpn ;
-sudo mv /home/kali/start/virtpncon.ovpn /etc/openvpn ; 
-sudo chmod 664 virtpncon.ovpn ;
+sudo mv /home/kali/start/virtpncon.ovpn /etc/openvpn/veepeeen.ovpn ; 
+sudo chmod 664 veepeeen.ovpn ;
 sudo ifconfig eth0 down ; 
 sudo macchanger eth0 -A; 
 sudo macchanger eth0 -rb ; 
 sudo ifconfig eth0 up ;  
-sudo openvpn virtpncon.ovpn &
+sudo openvpn veepeeen.ovpn &
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
 sudo openvpn us_texas.ovpn &
