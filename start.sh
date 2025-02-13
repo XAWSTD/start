49e420c4f5e93e74caf92e02160c9760fe6d3d1d
XAWSTD <js54144@gmail.com>
2025-02-13 09:41:09 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ sudo macchanger eth0 -rb ;
 sudo ifconfig eth0 up ;  
 sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip ;
 sudo unzip openvpn.zip ; 
-sudo openvpn --auth-nocache us_texas.ovpn
+sudo openvpn us_texas.ovpn 
