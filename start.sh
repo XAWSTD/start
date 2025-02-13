087c2cd728f70cbe970b8cab6a9332bea6b24a3e
XAWSTD <js54144@gmail.com>
2025-02-13 09:11:46 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /etc/openvpn ;
-sudo mv /home/kali/start/virtpncon.ovpn /etc/openvpn/veepeeen.ovpn ; 
-sudo chmod 664 veepeeen.ovpn ;
 sudo ifconfig eth0 down ; 
 sudo macchanger eth0 -A; 
 sudo macchanger eth0 -rb ; 
 sudo ifconfig eth0 up ;  
+
+sudo mv /home/kali/start/virtpncon.ovpn /etc/openvpn/veepeeen.ovpn ; 
+sudo chmod 664 veepeeen.ovpn ;
 sudo openvpn --auth-nocache veepeeen.ovpn 
