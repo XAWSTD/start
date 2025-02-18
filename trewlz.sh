87bd21a44b24fba05dacc0fcda1606f9ce7aa45e
XAWSTD <js54144@gmail.com>
2025-02-17 21:37:33 -0500

    Create trewlz.sh

---
+++ trewlz.sh
@@ -0,0 +1,2 @@
+cd /etc/trewlz ;
+pip install requests, sys, socket, os, paramiko, scapy , pwntools, cengal, GSMTC35, python-messaging, debdialer, remote-phone-control, keyboard, pymagi, hashlib, pyfiglet, 
