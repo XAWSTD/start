708b8c1a555841ea1799cafda6a53526072d45ec
XAWSTD <js54144@gmail.com>
2025-02-19 16:00:41 -0500

    Update trewlz.sh

--- trewlz.sh
+++ trewlz.sh
@@ -1,3 +1,4 @@
 cd /etc/trewlz ;
 pip install requests, sys, socket, os, paramiko, scapy , pwntools, cengal, GSMTC35, python-messaging, debdialer, remote-phone-control, keyboard, pymagi, hashlib, pyfiglet; 
 git clone https://github.com/0dayCTF/reverse-shell-generator ;
+wget -m https://book.hacktricks.wiki/en --convert-links --page-requisites 
